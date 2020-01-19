abb9f194f2b8a4b42abccf936a92b8bc4e37d85c
kkangil <kkangil94@naver.com>
2020-01-19 18:14:32 +0900

    200119 2.도구 다루기 - 1

---
+++ 2.도구 다루기/2-1.테스팅 프레임워크/TestFrameworks_01_test.js
@@ -0,0 +1,33 @@
+describe('createReservation(passenger, flight)', function () {
+  it('주어진 passenger를 passengerInfo 프로퍼티에 할당한다.', function () {
+    var testPassenger = {
+      firstName: '길동',
+      lastName: '홍'
+    };
+
+    var testFlight = {
+      number: '3443',
+      carrier: '대한항공',
+      destination: '울신'
+    };
+
+    var reservation = createReservation(testPassenger, testFlight);
+    expect(reservation.passengerInfo).toBe(testPassenger);
+  });
+
+  it('주어진 filght를 flightInfo 프로퍼티에 할당한다.', function () {
+    var testPassenger = {
+      firstName: '길동',
+      lastName: '홍'
+    };
+
+    var testFlight = {
+      number: '3443',
+      carrier: '대한항공',
+      destination: '울신'
+    };
+
+    var reservation = createReservation(testPassenger, testFlight);
+    expect(reservation.flightInfo).toBe(testFlight);
+  });
+});
